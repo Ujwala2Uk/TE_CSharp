c057765e75ae9ab590c2d85a5e9bcfa69a8aed7c
Ujwala2Uk <roopaumesha2@gmail.com>
2022-09-27 15:52:17 +0530

    commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+namespace Practice
+{
+    class Program
+    {
+        public static void Main()
+        {
+            sum();
+            Program m = new Program();
+            m.multi();
+        }
+
+        public static void sum()
+        {
+            int x = 10;
+            int y = 20;
+
+            Console.WriteLine(x + y);
+        }
+
+        public void multi()
+        {
+            int a = 10;
+            int b= 20;
+            int c = 30;
+            Console.WriteLine(a * b * c);
+        }
+    }
+}
